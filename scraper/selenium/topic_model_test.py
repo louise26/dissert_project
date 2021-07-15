404af134eb35654e68fcd8f1fa858af3869ba850
Louise Moizan <louisemoizan@gmail.com>
2021-07-15 16:35:06 +0100

    Cleaned up files and directories

--- scraper/selenium/topic_model_test.py
+++
@@ -1,9 +0,0 @@
-# Importing modules
-import pandas as pd
-from wordcloud import WordCloud
-
-# Read data into papers
-policy = pd.read_csv("Google.com.txt", delimiter = "\t")
-
-# Print head
-print(policy)
